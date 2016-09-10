f2900f86ba713720ea5f6131feb79946a727626a
Kosuke Takeda <june.bure@gmail.com>
2016-09-11 08:49:08 +0900

    layout ideas

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -4,7 +4,8 @@ class IdeasController < ApplicationController
   # GET /ideas
   # GET /ideas.json
   def index
-    @ideas = Idea.all
+    @ideas = Idea.limit 4
+    @tags = Tag.limit 10
   end
 
   # GET /ideas/1
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,3 +1,4 @@
 class Idea < ActiveRecord::Base
+  has_many :idea_images
   belongs_to :user
 end
