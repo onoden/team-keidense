f85f52cf5e8e0bb13f945d87e99ba4b883698ec1
Kosuke Takeda <june.bure@gmail.com>
2016-09-11 10:19:24 +0900

    add search controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :ideas
   resources :pictures
+  get 'search/:query' => 'search#search'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ test/controllers/search_controller_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class SearchControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+  def
+end
